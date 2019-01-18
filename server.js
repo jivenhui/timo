931befc0faa5f16774bc9cdef6f93ecd68c78b25
Jari <ji.venhuis@student.avans.nl>
2019-01-18 18:41:52 +0100

    aa

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/<to_your_project_name>'));
+app.use(express.static(__dirname + '/dist/brescollege'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
@@ -14,3 +14,4 @@ app.get('*', function(req,res) {
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
